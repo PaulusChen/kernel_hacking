3d10f1bb4f314f9ac6bbc27ebfb1a1ab6f6da18f
chenpeng <chenpeng@voole.com>
2018-02-08 22:57:52 +0800

    add syscall

--- hello.c
+++ hello.c
@@ -2,9 +2,22 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/slab.h>
+
+
+SYSCALL_DEFINE1(test_syscall,const char __user *, tag) {
+    
+    size_t size_of_tag = strlen(tag);
+    char *ntag = kmalloc(size_of_tag + 1,GFP_KERNEL);
+    copy_from_user(ntag, tag, size_of_tag);
+    ntag[size_of_tag] = '\0';
+    return 0;
+}
 
 static int hello_init(void) {
     printk(KERN_ALERT "I bear a charmed life. \n");
+    void *tp = kmalloc(16, GFP_KERNEL);
+    kfree(tp);
     return 0;
 }
 
