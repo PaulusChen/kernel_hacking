3f080dd89a2c878ea92c97b4a4aa9462486cac5e
chenpeng <chenpengsmail@qq.com>
2021-06-24 13:23:51 +0000

    update

--- automake.sh
+++ automake.sh
@@ -3,4 +3,5 @@
 # ./automake.sh ~/Documents/linux/
 
 KERNEL_PATH=$1
-make -C ${KERNEL_PATH} SUBDIRS=$PWD modules
+# make -C ${KERNEL_PATH} SUBDIRS=$PWD modules
+make -C ${KERNEL_PATH} M=$PWD modules
