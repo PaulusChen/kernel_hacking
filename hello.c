dcf1a3878fc7b7c68779840efae709607c49659f
chenpeng <chenpengsmail@qq.com>
2021-06-25 01:06:10 +0000

    update

--- hello.c
+++ hello.c
@@ -1,20 +1,21 @@
+#include <linux/init.h>   // module_init / module_exit
+#include <linux/module.h> // MODULE_*
+#include <linux/kernel.h> // printk
+#include <linux/slab.h>   // 提供kmalloc接口，基于对象的内存管理器，通过着色法增加行缓存命中率
 
-#include <linux/init.h>
-#include <linux/module.h>
-#include <linux/kernel.h>
-
-static int hello_init(void) {
+static int __init hello_init(void) {
     printk(KERN_ALERT "I bear a charmed life. \n");
+    void *tp = kmalloc(16, GFP_KERNEL); // 最大只能开辟128k-16 16个字节是被页描述符结构占用了
+    kfree(tp);
     return 0;
 }
 
-static void hello_exit(void) {
+static void __exit hello_exit(void) {
     printk(KERN_ALERT "Out, out, brief chandle! \n");
 }
 
 module_init(hello_init);
 module_exit(hello_exit);
-
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Paulus");
